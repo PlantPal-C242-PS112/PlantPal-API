5c13e032c1fb47dd0622eef8ae83ec791a703ab0
mhmmdriz <rizki25030@gmail.com>
2024-11-10 23:43:42 +0700

    Create table users migration

---
+++ prisma/migrations/20241110164316_create_table_users/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(255) NOT NULL,
+    `fullname` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `profile_photo` VARCHAR(255) NULL,
+    `otp` VARCHAR(5) NULL,
+    `otp_expiry` DATETIME(3) NULL,
+    `email_verified` BOOLEAN NOT NULL DEFAULT false,
+    `forgot_password_verified` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `users_username_key`(`username`),
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
