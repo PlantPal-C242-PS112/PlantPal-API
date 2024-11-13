eddf9d764c2572f8bef5463190a9272fcd5ced86
mhmmdriz <rizki25030@gmail.com>
2024-11-14 02:05:18 +0700

    Update package.json

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ const web = require('./application/web');
 const dotenv = require('dotenv');
 
 dotenv.config();
-const port = process.env.PORT;
+const port = 3000;
 
-web.listen(3000, () => {
+web.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
