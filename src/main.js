a6faf482cbffb3af77a571c8d24ef7266e007e6b
mhmmdriz <rizki25030@gmail.com>
2024-11-14 00:45:30 +0700

    Fix bug on CI/CD workflow

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 const port = process.env.PORT;
 
-web.listen(port, () => {
+web.listen(3000, () => {
   console.log(`Server is running on port ${port}`);
 });
